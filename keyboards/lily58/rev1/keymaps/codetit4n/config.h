a56fa65521f0e8f13ea7b83a8e1e5283e9523cd4
Lokesh Kumar <hello@lokeshkr.com>
2024-12-15 18:22:19 +0530

    lily58 codetit4n first ver

--- keyboards/lily58/rev1/keymaps/codetit4n/config.h
+++ keyboards/lily58/rev1/keymaps/codetit4n/config.h
@@ -23,6 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
 
+#define MASTER_LEFT
+
 #undef RGBLIGHT_LED_COUNT
 #define RGBLIGHT_EFFECT_BREATHING
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
