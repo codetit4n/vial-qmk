9e06efa7a00d374f1a0fb841660029d03702416f
Lokesh Kumar <hello@lokeshkr.com>
2024-12-16 10:05:02 +0530

    lily58 codetit4n lalt<>lctrl

--- keyboards/lily58/rev1/keymaps/codetit4n/keymap.c
+++ keyboards/lily58/rev1/keymaps/codetit4n/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, MO(_LOWER), MO(_RAISE), KC_RALT
+                        KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, KC_ENT, MO(_LOWER), MO(_RAISE), KC_RALT
 ),
 
 [_LOWER] = LAYOUT(
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F11,    KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                  KC_6,   KC_7,   KC_8,   KC_9,  KC_0,  KC_F12,
   KC_TAB, XXXXXXX, KC_S,   XXXXXXX, XXXXXXX,  XXXXXXX,                   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_SCLN, KC_QUOT,
   KC_LSFT, XXXXXXX, XXXXXXX, KC_C, XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_MINS, KC_EQL, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, XXXXXXX, XXXXXXX, KC_RALT
+                        KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, KC_ENT, MO(_LOWER), MO(_RAISE), KC_RALT
 ),
 
 [_RAISE] = LAYOUT(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,  KC_EXLM,    KC_AT,    KC_HASH,    KC_DLR,    KC_PERC,                     KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_PIPE,
         KC_TAB,  XXXXXXX,    KC_S,   XXXXXXX,   KC_MS_BTN1,   KC_MS_BTN2,                       MS_LEFT, MS_DOWN, MS_UP, MS_RGHT,   KC_COLN, KC_DQT,
         KC_LSFT,   XXXXXXX,   XXXXXXX,   KC_C,  XXXXXXX,  XXXXXXX,   KC_LCBR, KC_RBRC,  KC_UNDS,    KC_PLUS,    KC_LT, KC_GT,  KC_QUES,  KC_RSFT,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, XXXXXXX, XXXXXXX, KC_RALT
+        KC_LALT, KC_LCTL, KC_LGUI, KC_SPC, KC_ENT, MO(_LOWER), MO(_RAISE), KC_RALT
     ),
 [_ADJUST] = LAYOUT(
         KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
